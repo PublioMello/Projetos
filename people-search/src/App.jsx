0e2eae2a03d15c08a7ac5c0d8ccf4ce877a6beec
Publio Filho <publio.lima.mello@gmail.com>
2026-08-18 07:16:14 -0300

    new peojwct

---
+++ people-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "./components/Head/Head";
+
+const App = () => {
+  return (
+    <div>
+      <Head />
+    </div>
+  );
+};
+
+export default App;
---
+++ people-search/src/components/Head/Head.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Head() {
+  return (
+    <div>
+      <h1>SearchX</h1>
+      <p>Research people free </p>
+    </div>
+  );
+}
+
+export default Head;
